d6079e7d19f171560d92d239a51c520a354a40de
Gulbala Salamov <g.salamov@gmail.com>
2022-07-20 19:51:57 +0300

    [Project created] Initial commit

---
+++ src/main/java/com/gulbalasalamov/patikatodebjavaspringbootcampprojeodevlerigulbalasalamov/PatikaTodebJavaspringbootcampProjeOdevleriGulbalasalamovApplication.java
@@ -0,0 +1,13 @@
+package com.gulbalasalamov.patikatodebjavaspringbootcampprojeodevlerigulbalasalamov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaTodebJavaspringbootcampProjeOdevleriGulbalasalamovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikaTodebJavaspringbootcampProjeOdevleriGulbalasalamovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gulbalasalamov/patikatodebjavaspringbootcampprojeodevlerigulbalasalamov/PatikaTodebJavaspringbootcampProjeOdevleriGulbalasalamovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulbalasalamov.patikatodebjavaspringbootcampprojeodevlerigulbalasalamov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaTodebJavaspringbootcampProjeOdevleriGulbalasalamovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
