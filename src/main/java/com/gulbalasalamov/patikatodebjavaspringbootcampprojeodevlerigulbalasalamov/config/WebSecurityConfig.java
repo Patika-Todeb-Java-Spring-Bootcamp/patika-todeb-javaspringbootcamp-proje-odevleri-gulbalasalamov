5440031f54208d299952731f05109c2cdcb42a31
Gulbala Salamov <g.salamov@gmail.com>
2022-08-01 16:03:57 +0300

    Security classes added.

--- src/main/java/com/gulbalasalamov/patikatodebjavaspringbootcampprojeodevlerigulbalasalamov/config/WebSecurityConfig.java
+++ src/main/java/com/gulbalasalamov/patikatodebjavaspringbootcampprojeodevlerigulbalasalamov/config/WebSecurityConfig.java
@@ -1,15 +1,12 @@
-package com.gulbalasalamov.patikatodebjavaspringbootcampprojeodevlerigulbalasalamov.security;
+package com.gulbalasalamov.patikatodebjavaspringbootcampprojeodevlerigulbalasalamov.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     //WebSecurityConfigurer creates beans, makes configs, gets https... creates auth layer
 
     @Override
